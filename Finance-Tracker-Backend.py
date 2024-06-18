b4dd903d129173857b1a75506708928ec8adb123
Cris Mendez <mende036@rangers.uwp.edu>
2024-06-18 13:41:24 -0500

    Added Function Skeletons

--- Finance-Tracker-Backend.py
+++ Finance-Tracker-Backend.py
@@ -1,5 +1,20 @@
 import csv
 
-with open('Finance-Data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Income', 'Expenses']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# with open('Finance-Data.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['Income', 'Expense', 'Amount']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#     writer.writeheader()
+
+def add_expense(name, amount):
+    with open('Finance-Data.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Income', 'Expense', 'Amount']
+        writer = csv.DictWriter(csvfile, fieldnames)
+        writer.writerow({'Income': 'N/A', 'Expense': name, 'Amount': amount})
+def remove_expense(name, amount):
+    return 0
+def add_income():
+    return 0
+def remove_income():
+    return 0
+add_expense("Test Expense", 10.00)
