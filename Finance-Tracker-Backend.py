1bcfceca0ad8688f1e815708340300b611e81fa7
Cris Mendez <mende036@rangers.uwp.edu>
2024-06-20 15:30:50 -0500

    GUI changes

--- Finance-Tracker-Backend.py
+++ Finance-Tracker-Backend.py
@@ -7,20 +7,21 @@ import pandas as pd
 
 colnames = ['ID','Income','Expense','Amount']
 df = pd.read_csv('Finance-Data.csv', names=colnames, header=None, index_col='ID')
-
+print(df.head)
 def add_expense(id, expense, amount):
     data = {
-        'ID': id,
-        'Income': 'N/A',
-        'Expense': expense,
-        'Amount': amount
+        'ID': [id],
+        'Income': ['N/A'],
+        'Expense': [expense],
+        'Amount': [amount]
     }
     df = pd.DataFrame(data)
-    df.to_csv('Finance-Data.csv', mode='a') 
-def remove_expense():
-    return 0
+    df.to_csv('Finance-Data.csv', mode='a', header=False, index=False) 
+def remove_expense(id):
+    global df
+    df.drop(index=pd.int(id))
 def add_income():
     return 0
 def remove_income():
     return 0
-add_expense('01','Dining',10.00)
+remove_expense('01')
--- Personal-Finance-Tracker.py
+++ Personal-Finance-Tracker.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding=100)
 frm.grid()
 ttk.Label(frm, text="Personal Finance Tracker").grid(column=0,row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0,row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=0)
+ttk.Button(frm, text="Add Expense").grid(column=0,row=2)
+ttk.Entry(frm).grid(column=0,row=1)
+ttk.Button(frm, text="Add Income").grid(column=0,row=4)
+ttk.Entry(frm).grid(column=0,row=3)
 root.mainloop()
