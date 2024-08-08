e569edd403bb9d42c0f858674b9f6dd357d4278b
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-07 23:16:08 -0500

    Update Finance-Tracker-Backend.py

--- Finance-Tracker-Backend.py
+++ Finance-Tracker-Backend.py
@@ -2,26 +2,18 @@
 Class that will take care of the backend of the finance tracker.
 """
 
-import csv
-import pandas as pd
+import sqlite3
 
-colnames = ['ID','Income','Expense','Amount']
-df = pd.read_csv('Finance-Data.csv', names=colnames, header=None, index_col='ID')
-print(df.head)
-def add_expense(id, expense, amount):
-    data = {
-        'ID': [id],
-        'Income': ['N/A'],
-        'Expense': [expense],
-        'Amount': [amount]
-    }
-    df = pd.DataFrame(data)
-    df.to_csv('Finance-Data.csv', mode='a', header=False, index=False) 
-def remove_expense(id):
-    global df
-    df.drop(index=pd.int(id))
-def add_income():
-    return 0
-def remove_income():
-    return 0
-remove_expense('01')
+class FinanceTrackerBackend:
+    def __init__(self):
+        mydb = sqlite3.connector.connect('Finances.db')
+        cursor = mydb.cursor()
+        print('Database initialized')
+    def add_expense(id, expense, amount):
+        return 0
+    def remove_expense(id):
+        return 0
+    def add_income():
+        return 0
+    def remove_income():
+        return 0
