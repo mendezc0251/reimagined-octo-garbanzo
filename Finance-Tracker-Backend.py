cf7365ac9573fc3c7dbfc776ad31b2bbf908aa09
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-10 20:04:09 -0500

    Added insert and delete statements

--- Finance-Tracker-Backend.py
+++ Finance-Tracker-Backend.py
@@ -5,15 +5,17 @@ Class that will take care of the backend of the finance tracker.
 import sqlite3
 
 class FinanceTrackerBackend:
-    def __init__(self):
-        mydb = sqlite3.connector.connect('Finances.db')
-        cursor = mydb.cursor()
-        print('Database initialized')
+    mydb = sqlite3.connector.connect('Finances.db')
+    cursor = mydb.cursor()
+    print('Database initialized')
     def add_expense(id, expense, amount):
+        cursor.execute('INSERT INTO Expenses VALUES(id, expense, amount)')
         return 0
     def remove_expense(id):
+        cursor.execute('DELETE FROM Expenses WHERE id = id')
         return 0
     def add_income():
+        cursor.
         return 0
     def remove_income():
         return 0
