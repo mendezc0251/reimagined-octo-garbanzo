f1e258d763f04beddca84b878f42327b2bfe2a63
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-12 17:05:45 -0500

    Update Finance-Tracker-Backend.py

--- Finance-Tracker-Backend.py
+++ Finance-Tracker-Backend.py
@@ -4,19 +4,25 @@ Class that will take care of the backend of the finance tracker.
 
 import sqlite3
 
-class FinanceTrackerBackend:
-    mydb = sqlite3.connector.connect('Finances.db')
-    cursor = mydb.cursor()
-    print('Database initialized')
+
+class FinanceTracker:
+    def __init__(self):
+        self.mydb = sqlite3.connector.connect('Finances.db')
+        self.cursor = self.mydb.cursor()
+        print('Database initialized')
+
     def add_expense(id, expense, amount):
-        FinanceTrackerBackend.cursor.execute('INSERT INTO Expenses VALUES(id, expense, amount)')
+        cursor.execute('INSERT INTO Expenses VALUES(id, expense, amount)')
         return 0
+
     def remove_expense(id):
-        FinanceTrackerBackend.cursor.execute('DELETE FROM Expenses WHERE id = id')
+        cursor.execute('DELETE FROM Expenses WHERE id = id')
         return 0
-    def add_income():
-        FinanceTrackerBackend.cursor.execute('INSERT INTO Incomes VALUES(id, income, amount)')
+
+    def add_income(id, income, amount):
+        cursor.execute('INSERT INTO Income VALUES(id, income, amount)')
         return 0
-    def remove_income(self):
-        FinanceTrackerBackend.cursor.execute('DELETE FROM Incomes WHERE id = id')
+
+    def remove_income(id):
+        cursor.execute('DELETE FROM Income WHERE id = id')
         return 0
