b8baea257f10ebcda5915d1ec04ab3ebe0d9f100
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-13 11:56:14 -0500

    Added more functionality to frontend

--- Personal-Finance-Tracker.py
+++ Personal-Finance-Tracker.py
@@ -1,12 +1,25 @@
+from Backend import FinanceTracker
 from tkinter import *
 from tkinter import ttk
+class PersonalFinanceTracker:
+    def __init__(self, root):
+        frm = ttk.Frame(root, padding=100)
+        frm.grid()
+        root.title("Personal Finance Tracker")
+        ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=5)
+        ttk.Button(frm, text="Add Expense").grid(column=1,row=2)
+        e_1 = ttk.Entry(frm).grid(column=0,row=1)
+        e_1.pack()
+        e_2 = ttk.Entry(frm).grid(column=1,row=1)
+        e_2.pack()
+        e_3 = ttk.Entry(frm).grid(column=2,row=1)
+        e_3.pack()
+        ttk.Button(frm, text="Add Income").grid(column=1,row=4)
+        ttk.Entry(frm).grid(column=0,row=3)
+        ttk.Entry(frm).grid(column=1,row=3)
+        ttk.Entry(frm).grid(column=2,row=3)
+    def all_expenses():
+
 root = Tk()
-frm = ttk.Frame(root, padding=100)
-frm.grid()
-ttk.Label(frm, text="Personal Finance Tracker").grid(column=0,row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=0)
-ttk.Button(frm, text="Add Expense").grid(column=0,row=2)
-ttk.Entry(frm).grid(column=0,row=1)
-ttk.Button(frm, text="Add Income").grid(column=0,row=4)
-ttk.Entry(frm).grid(column=0,row=3)
+PersonalFinanceTracker(root)
 root.mainloop()
