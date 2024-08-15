db35b1427231267edc52e69f4e221131b039c537
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-15 15:29:21 -0500

    Added comments, and functionality to front and backends

--- Backend.py
+++ Backend.py
@@ -6,19 +6,24 @@ import sqlite3
 
 
 class FinanceTracker:
+    # This function initializes the Finance Trackers backend
     def __init__(self):
-        self.mydb = sqlite3.connector.connect('Finances.db')
+        self.mydb = sqlite3.connect('Finances.db')
         self.cursor = self.mydb.cursor()
         print('Database initialized')
-
+    # This function adds an expense to the Finances databases Expenses Table
     def add_expense(self, id, expense, amount):
-        self.cursor.execute('INSERT INTO Expenses VALUES(id, expense, amount)')
-
+        self.cursor.execute('INSERT INTO Expenses VALUES(?, ?, ?)',(id,expense,amount))
+        self.mydb.commit()
+    # This function removes an expense from the Expenses Table
     def remove_expense(self, id):
-        self.cursor.execute('DELETE FROM Expenses WHERE id = id')
-
+        self.cursor.execute('DELETE FROM Expenses WHERE id = ?'(id))
+        self.mydb.commit()
+    # This function adds and income to the Finances databases Incomes Table
     def add_income(self, id, income, amount):
-        self.cursor.execute('INSERT INTO Income VALUES(id, income, amount)')
-
+        self.cursor.execute('INSERT INTO Income VALUES(?, ?, ?)'(id,income,amount))
+        self.mydb.commit()
+    # This function removes an income from the Incomes Table
     def remove_income(self, id):
-        self.cursor.execute('DELETE FROM Income WHERE id = id')
+        self.cursor.execute('DELETE FROM Income WHERE id = ?'(id))
+        self.mydb.commit()
--- Personal-Finance-Tracker.py
+++ Personal-Finance-Tracker.py
@@ -1,28 +1,41 @@
+"""
+Class that will handle the gui and all of the Finance Trackers frontend.
+"""
 from Backend import FinanceTracker
 from tkinter import *
 from tkinter import ttk
 
 class PersonalFinanceTracker:
+    # This function initializes the Finance Trackers frontend
     def __init__(self, root):
         frm = ttk.Frame(root, padding=100)
         frm.grid()
         root.title("Personal Finance Tracker")
         ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=5)
-        ttk.Button(frm, text="Add Expense").grid(column=1,row=2)
-        self.e_1 = ttk.Entry(frm).grid(column=0,row=1)
-        self.e_1.pack()
-        self.e_2 = ttk.Entry(frm).grid(column=1,row=1)
-        self.e_2.pack()
-        self.e_3 = ttk.Entry(frm).grid(column=2,row=1)
-        self.e_3.pack()
-        ttk.Button(frm, text="Add Income").grid(column=1,row=4)
-        ttk.Entry(frm).grid(column=0,row=3)
-        ttk.Entry(frm).grid(column=1,row=3)
-        ttk.Entry(frm).grid(column=2,row=3)
+        ttk.Button(frm, text="Add Expense", command=self.all_expenses).grid(column=1,row=2)
+        self.e_2 = ttk.Entry(frm)
+        self.e_2.grid(column=1,row=1)
+        self.e_3 = ttk.Entry(frm)
+        self.e_3.grid(column=2,row=1)
+        ttk.Button(frm, text="Add Income", command=self.all_incomes).grid(column=1,row=4)
+        self.i_2 = ttk.Entry(frm)
+        self.i_2.grid(column=1,row=3)
+        self.i_3 = ttk.Entry(frm)
+        self.i_3.grid(column=2,row=3)
+    # This function adds a users input expense to the Finances database
     def all_expenses(self, *args):
         try:
-            FinanceTracker.add_expense(self.e_1.get(), self.e_2.get(), self.e_3.get())
-        except
+            f = FinanceTracker()
+            f.add_expense(self.e_1.get(), self.e_2.get(), self.e_3.get())
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    # This function adss a users input income to the Finances database
+    def all_incomes(self, *args):
+        try:
+            f = FinanceTracker()
+            f.add_income(self.i_1.get(), self.i_2.get(), self.i_3.get())
+        except Exception as e:
+            print(f"An error occured: {e}")
 root = Tk()
 PersonalFinanceTracker(root)
 root.mainloop()
