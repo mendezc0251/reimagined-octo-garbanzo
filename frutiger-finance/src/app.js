b8eba9ba0b1482fd31c38c81258ad902ddeb1505
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-19 13:37:00 -0500

    added button and styles

--- frutiger-finance/src/app.js
+++ frutiger-finance/src/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./styles.css"
 function WelcomeButton(){
     return(
-        <button>Welcome to Frutiger Finance!</button>
+        <button className='styles.welbutton'>Welcome to Frutiger Finance!</button>
     )
 }
 export default function MyApp(){
