c431801732918530837cb8938a0282dcb46700d4
Cris Mendez <mende036@rangers.uwp.edu>
2024-08-19 13:21:01 -0500

    Refactor files

---
+++ frutiger-finance/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function WelcomeButton(){
+    return(
+        <button>Welcome to Frutiger Finance!</button>
+    )
+}
+export default function MyApp(){
+    return(
+        <div>
+            <WelcomeButton/>
+        </div>
+    )
+}
--- frutiger-finance/src/index.js
+++ frutiger-finance/src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from "react";
+import {createRoot} from "react-dom/client"
+import "./styles.css"
 
-const myFirstElement = <h1>Hello React!</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+import App from "./App"
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+)
